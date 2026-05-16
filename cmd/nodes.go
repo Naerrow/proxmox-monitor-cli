27435dd6c3f446eb08480bc62d7674a4f140e1ee
Naerrow <jaehee3505@naver.com>
2026-05-16 21:12:11 +0900

    feat: nodes 명령어 구현

--- cmd/nodes.go
+++ cmd/nodes.go
@@ -2,7 +2,11 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/naerrow/proxmox-monitor-cli/config"
+	"github.com/naerrow/proxmox-monitor-cli/internal/proxmox"
 	"github.com/spf13/cobra"
 )
 
@@ -10,6 +14,30 @@ var nodesCmd = &cobra.Command{
 	Use:   "nodes",
 	Short: "노드 목록 (CPU/메모리 포함)",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("TODO: nodes 명령어 구현 예정")
+		cfg := config.Load()
+		client := proxmox.NewClient(cfg.ProxmoxURL, cfg.ProxmoxToken, cfg.ProxmoxNode)
+
+		nodes, err := client.GetNodes()
+		if err != nil {
+			fmt.Println("❌ 오류:", err)
+			os.Exit(1)
+		}
+
+		t := table.NewWriter()
+		t.SetOutputMirror(os.Stdout)
+		t.AppendHeader(table.Row{"노드", "상태", "CPU 사용률", "메모리 사용량", "업타임"})
+
+		for _, n := range nodes {
+			cpuPercent := fmt.Sprintf("%.1f%%", n.CPU*100)
+			memUsed := n.Mem / 1024 / 1024 / 1024
+			memTotal := n.MaxMem / 1024 / 1024 / 1024
+			mem := fmt.Sprintf("%dGB / %dGB", memUsed, memTotal)
+			uptime := fmt.Sprintf("%dd %dh", n.Uptime/86400, (n.Uptime%86400)/3600)
+
+			t.AppendRow(table.Row{n.Node, n.Status, cpuPercent, mem, uptime})
+		}
+
+		t.SetStyle(table.StyleLight)
+		t.Render()
 	},
 }
